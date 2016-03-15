fc66bfa0a55b0ba85212587d106a37cf451633ac
Olaf Alders <olaf@wundersolutions.com>
2016-03-15 16:21:52 -0400

    Initial commit.

---
+++ lib/Plack/Middleware/ExtractedStackTrace.pm
@@ -0,0 +1,102 @@
+package Plack::Middleware::ExtractedStackTrace;
+
+use strict;
+use warnings;
+
+use parent qw/Plack::Middleware/;
+
+our $VERSION = '1.000000';
+
+use Devel::StackTrace;
+use Devel::StackTrace::Extract qw( extract_stack_trace );
+use Devel::StackTrace::AsHTML;
+use Try::Tiny qw( catch try );
+use Plack::Util::Accessor qw( force no_print_errors );
+
+sub call {
+    my ( $self, $env ) = @_;
+
+    my $trace;
+    my $caught;
+
+    my $res = try {
+        $self->app->($env);
+    }
+    catch {
+        $caught = $_;
+        [
+            500, [ 'Content-Type', 'text/plain; charset=utf-8' ],
+            [ no_trace_error( utf8_safe($caught) ) ]
+        ];
+    };
+
+    $trace = extract_stack_trace($caught) if $caught;
+
+    if (
+        $trace
+        && ( $caught
+            || ( $self->force && ref $res eq 'ARRAY' && $res->[0] == 500 ) )
+        ) {
+        my $text = $trace->as_string;
+        my $html = $trace->as_html;
+        $env->{'plack.stacktrace.text'} = $text;
+        $env->{'plack.stacktrace.html'} = $html;
+        $env->{'psgi.errors'}->print($text) unless $self->no_print_errors;
+        if ( ( $env->{HTTP_ACCEPT} || '*/*' ) =~ /html/ ) {
+            $res = [
+                500, [ 'Content-Type' => 'text/html; charset=utf-8' ],
+                [ utf8_safe($html) ]
+            ];
+        }
+        else {
+            $res = [
+                500, [ 'Content-Type' => 'text/plain; charset=utf-8' ],
+                [ utf8_safe($text) ]
+            ];
+        }
+    }
+
+    return $res;
+}
+
+sub no_trace_error {
+    my $msg = shift;
+    chomp($msg);
+
+    return <<"EOF";
+The application raised the following error:
+
+  $msg
+
+For which no stack trace was captured.
+EOF
+}
+
+sub utf8_safe {
+    my $str = shift;
+
+    # NOTE: I know messing with utf8:: in the code is WRONG, but
+    # because we're running someone else's code that we can't
+    # guarantee which encoding an exception is encoded, there's no
+    # better way than doing this. The latest Devel::StackTrace::AsHTML
+    # (0.08 or later) encodes high-bit chars as HTML entities, so this
+    # path won't be executed.
+    ## no critic (Subroutines::ProhibitCallsToUnexportedSubs)
+    ## no critic (Modules::RequireExplicitInclusion)
+    if ( utf8::is_utf8($str) ) {
+        utf8::encode($str);
+    }
+    ## use critic
+
+    $str;
+}
+
+=head1 ACKNOWLEDGEMENTS
+
+Parts of this code (in this module file only) were derived from
+L<Plack::MiddleWare::StackTrace>, part of the Plack distribution.
+
+=cut
+
+1;
+
